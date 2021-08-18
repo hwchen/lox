ced5ac2cfbfb0f49c4ca493d0745902a0b2822e5
Walther Chen <walther.chen@gmail.com>
2021-08-18 01:33:48 -0400

    runFile and runPrompt

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,14 @@
 const clap = @import("clap");
 const std = @import("std");
 const io = std.io;
+const fs = std.fs;
+const Allocator = std.mem.Allocator;
 
 pub fn main() anyerror!void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var alloc = &gpa.allocator;
+
     const params = comptime [_]clap.Param(clap.Help){
         clap.parseParam("-h, --help             Display this help and exit.              ") catch unreachable,
         clap.parseParam("<PATH>                 Execute file path.                       ") catch unreachable,
@@ -23,7 +29,46 @@ pub fn main() anyerror!void {
         try clap.help(std.io.getStdOut().writer(), &params);
     }
 
-    for (args.positionals()) |pos| {
-        std.log.info("{s}", .{pos});
+    if (args.positionals().len == 0) {
+        try runPrompt();
+    } else if (args.positionals().len == 1) {
+        try runFile(alloc, args.positionals()[0]);
+    } else {
+        return error.IncorrectArg;
     }
+}
+
+fn runFile(alloc: *Allocator, path: []const u8) !void {
+    const file = try std.fs.openFileAbsolute(path, .{ .read = true });
+    defer file.close();
+
+    const max_size = 1024 * 8;
+    const bytes = try file.readToEndAlloc(alloc, max_size);
+    defer alloc.free(bytes);
+
+    return run(bytes);
+}
+
+fn runPrompt() !void {
+    var buf: [1024]u8 = undefined;
+    var stdin = io.getStdIn().reader();
+    var rdr = io.bufferedReader(stdin).reader();
+    var stdout = io.getStdOut().writer();
+    var stdout_buf = io.bufferedWriter(stdout);
+    var wtr = stdout_buf.writer();
+
+    while (true) {
+        _ = try wtr.write("> ");
+        try stdout_buf.flush();
+
+        if (try rdr.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+            try run(line);
+        } else {
+            break;
+        }
+    }
+}
+
+fn run(bytes: []const u8) !void {
+    _ = bytes;
 }
