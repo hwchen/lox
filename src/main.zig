25dd5be70d70f9d9c755d51b047f5c676549d3d6
Walther Chen <walther.chen@gmail.com>
2021-08-18 12:03:52 -0400

    error reporting and early return

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ const io = std.io;
 const fs = std.fs;
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
+const Writer = std.io.Writer;
 
 pub fn main() anyerror!void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -43,8 +44,6 @@ pub fn main() anyerror!void {
 
 /// Lox is main entry point, and holds state for a session
 const Lox = struct {
-    hadError: bool = false,
-
     fn runFile(self: *Lox, alloc: *Allocator, path: []const u8) !void {
         _ = self;
         const file = try std.fs.cwd().openFile(path, .{ .read = true });
@@ -54,7 +53,13 @@ const Lox = struct {
         const bytes = try file.readToEndAlloc(alloc, max_size);
         defer alloc.free(bytes);
 
-        return self.run(alloc, bytes);
+        var lox_err: ?*LoxError = null;
+        try self.run(alloc, bytes, lox_err);
+        if (lox_err) |err| {
+            err.write_report();
+            err.deinit();
+            std.os.exit(65);
+        }
     }
 
     fn runPrompt(self: *Lox, alloc: *Allocator) !void {
@@ -71,7 +76,12 @@ const Lox = struct {
             try stdout_buf.flush();
 
             if (try rdr.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-                try self.run(alloc, line);
+                var lox_err: ?*LoxError = null;
+                try self.run(alloc, line, lox_err);
+                if (lox_err) |err| {
+                    err.write_report();
+                    err.deinit();
+                }
             } else {
                 // EOF
                 break;
@@ -79,11 +89,19 @@ const Lox = struct {
         }
     }
 
-    fn run(self: *Lox, alloc: *Allocator, bytes: []const u8) !void {
+    fn run(self: *Lox, alloc: *Allocator, bytes: []const u8, lox_err: ?*LoxError) !void {
         _ = self;
         var scanner = Scanner{ .alloc = alloc, .bytes = bytes };
-        const tokens = scanner.scanTokens();
+        var scanner_err: ?*ScannerError = null;
+
+        const tokens = scanner.scanTokens(scanner_err);
         defer tokens.deinit();
+
+        if (scanner_err) |err| {
+            // Is this correct, is the error moved?
+            lox_err.?.* = LoxError{ .scanner = err.* };
+            return;
+        }
 
         for (tokens.items) |token| {
             std.log.info("{}", .{token});
@@ -91,12 +109,42 @@ const Lox = struct {
     }
 };
 
+const LoxError = union(enum) {
+    scanner: ScannerError,
+
+    fn write_report(self: LoxError) void {
+        switch (self) {
+            .scanner => |err| err.write_report(),
+        }
+    }
+
+    fn deinit(self: *LoxError) void {
+        switch (self.*) {
+            .scanner => |*err| err.deinit(),
+        }
+    }
+};
+
+const ScannerError = struct {
+    line: u64,
+    message: ArrayList(u8),
+
+    fn write_report(self: ScannerError) void {
+        std.debug.print("[line {d}] Error: {s}", .{ self.line, self.message });
+    }
+
+    fn deinit(self: *ScannerError) void {
+        self.message.deinit();
+    }
+};
+
 const Scanner = struct {
     alloc: *Allocator,
     bytes: []const u8,
 
-    fn scanTokens(self: *Scanner) ArrayList(Token) {
+    fn scanTokens(self: *Scanner, err: ?*ScannerError) ArrayList(Token) {
         _ = self;
+        _ = err;
         return ArrayList(Token).init(self.alloc);
     }
 };
