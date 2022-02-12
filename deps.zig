78e60103f0ed52576f5b5a4b7467318b51048cdc
Walther Chen <walther.chen@gmail.com>
2022-02-11 20:35:48 -0500

    update zig-clap for zig 0.9.0

--- deps.zig
+++ deps.zig
@@ -6,7 +6,7 @@ pub const pkgs = struct {
     pub const clap = Pkg{
         .name = "clap",
         .path = FileSource{
-            .path = ".gyro/zig-clap-Hejsil-github.com-c5fb2282/pkg/clap.zig",
+            .path = ".gyro/zig-clap-Hejsil-github.com-cf8a34d1/pkg/clap.zig",
         },
     };
 
